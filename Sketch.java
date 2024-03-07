aac6798f9bc377d8ab98937d96067c8b0ad685c7
alizawqr <aliza.waqar25@ycdsbk12.ca>
2024-03-07 14:21:37 -0500

    move lamp

--- Sketch.java
+++ Sketch.java
@@ -6,31 +6,72 @@ public class Sketch extends PApplet {
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
+
   public void settings() {
-	// put your size call here
-    size(400, 400);
-  }
-
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
+    // put your size call here
+    size(500, 500);
+    }
+    
   public void setup() {
-    background(210, 255, 173);
-  }
+    background(227, 209, 237);
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
+    float randomX = random(0, 500);
+    float randomY = random(0, 500);
+
+    if (randomX < 250 || randomY < 250){
+      // Light
+      stroke(125);
+      strokeWeight(3);
+      fill(252, 239, 154);
+      ellipse(randomX, randomY, 120, 120);
+
+      // Shade
+      fill(125);
+      rect(randomX + 10, randomY - 60, 55, 120);
+
+      // Stand
+      strokeWeight(15);
+      line(randomX + 20, randomY + 60, randomX + 20, randomY + 240);
+
+      // Bottom
+      rect(randomX - 85, randomY + 240, 200, 30);
+
+
+    }
+    else if (randomX > 250 || randomY > 250){
+      // Light
+      stroke(125);
+      strokeWeight(3);
+      fill(252, 239, 154);
+      ellipse(randomX, randomY, 120, 120);
+
+      // Shade
+      fill(125);
+      rect(randomX + 10, randomY - 60, 55, 120);
+
+      // Stand
+      strokeWeight(15);
+      line(randomX + 20, randomY + 60, randomX + 20, randomY + 240);
+
+      // Bottom
+      rect(randomX - 85, randomY + 240, 200, 30);
+    }
+    }
+    
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
-  
-  // define other methods down here.
+    // Define time variables
+    int hr = hour();
+    int min = minute();
+
+    // Set time
+    fill(0);
+    textSize(75);
+    text(hr, 100, 150);
+    text(min, 100, 250);
+
+        
+        
+}
+
 }
